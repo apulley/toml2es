b83128a217031f4f1a21a7ee51c02a4d93b21e88
Andrew <andrewpulley@gmail.com>
2018-09-24 23:04:13 -0500

    node build

--- scripts/build.js
+++ scripts/build.js
@@ -3,7 +3,11 @@ const path = require('path');
 const concat = require('concat-stream');
 const toml = require('toml');
 
-fs.createReadStream('content/page.toml', 'utf8').pipe(concat(function(data) {
-  var parsed = toml.parse(data);
-  console.log(parsed)
+fs.createReadStream('toml/page.toml', 'utf8').pipe(concat(function(data) {
+  let parsed = toml.parse(data);
+  let content = JSON.stringify(parsed);
+  fs.writeFile(`content/index.js`, content, {flag: 'w'}, function (error){ 
+    if (error) return console.log(error);
+  })
+  console.log(content)
 }));
