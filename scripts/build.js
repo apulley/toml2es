296e8f6050fe25588f17b83c68c4ad67f97cd5f5
Andrew <andrewpulley@gmail.com>
2018-09-28 22:02:39 -0500

    updates

--- scripts/build.js
+++ scripts/build.js
@@ -6,7 +6,6 @@ const toml = require('toml');
 const beautify = require('js-beautify').js;
 
 const rxFileName = /([^\/]+)(?=\.\w+$)/;
-let tomlFiles = [];
 
 glob('toml/*.toml', {
   nodir: true,
@@ -18,15 +17,18 @@ glob('toml/*.toml', {
 
 function buildModules(tomlPath){
   fs.createReadStream(tomlPath, 'utf8').pipe(concat( function(data) {
-    let parsed = toml.parse(data);
     let content = beautify(
-      `export default ${JSON.stringify(parsed)}`,
-      { end_with_newline: true, indent_size: 2, space_in_empty_paren: true
-    });
+      `export default ${JSON.stringify(toml.parse(data))}`,
+      { end_with_newline: true, indent_size: 2, space_in_empty_paren: true});
 
-    fs.writeFile(`content/${tomlPath.match(rxFileName)[0]}.js`, content, {flag: 'w'}, function (error){ 
-      if (error) return console.log(error);
-    });
+    fs.writeFile(
+      `content/${tomlPath.match(rxFileName)[0]}.js`, 
+      content, 
+      {flag: 'w'}, 
+      function (error){ 
+        if (error) return console.log(error);
+      }
+    );
   }));
 }
 
