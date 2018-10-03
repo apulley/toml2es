8bdd0d6d3a3707a919b7fbae615c124705a6c6d9
Andrew <andrewpulley@gmail.com>
2018-10-03 00:46:57 -0500

    updates to build

--- scripts/build.js
+++ scripts/build.js
@@ -6,30 +6,50 @@ const toml = require('toml');
 const beautify = require('js-beautify').js;
 
 const rxFileName = /([^\/]+)(?=\.\w+$)/;
+let contentJson = {};
 
 glob('toml/*.toml', {
   nodir: true,
 }, function(err, files){
-  files.forEach(function(path){
-    buildModules(path);
-  });
+    buildModules(files, myFunction);
 });
 
-function buildModules(tomlPath){
-  fs.createReadStream(tomlPath, 'utf8').pipe(concat( function(data) {
-    let content = beautify(
-      `export default ${JSON.stringify(toml.parse(data))}`,
-      { end_with_newline: true, indent_size: 2, space_in_empty_paren: true});
+function buildModules(tomlPaths, callback){
+  let files = [];
+  var actions = tomlPaths.map(path => {
+    return new Promise(function(resolve, reject){
+      fs.createReadStream(path, 'utf8').pipe(concat( data => {
+        let content = beautify(
+          `${JSON.stringify(toml.parse(data))}`,
+          { end_with_newline: true, indent_size: 2, space_in_empty_paren: true}
+        );
+        const fileName = path.match(rxFileName)[0];
 
-    fs.writeFile(
-      `content/${tomlPath.match(rxFileName)[0]}.js`, 
-      content, 
-      {flag: 'w'}, 
-      function (error){ 
-        if (error) return console.log(error);
-      }
-    );
-  }));
+        fs.writeFile(
+          `content/${fileName}.js`, 
+          `export default ${content}`, 
+          {flag: 'w'}, 
+          error => { 
+            if (error) {
+              reject(error);
+            }
+            resolve();
+          }
+        );
+        files.push({content, fileName});
+      }));
+    });
+  });
+  Promise.all(actions).then(() => {
+    let jsonObj = {};
+    files.forEach( jsModule => {
+      jsonObj[jsModule.fileName] = JSON.parse(jsModule.content);
+    });
+    callback(jsonObj);
+  });
 }
 
+function myFunction(data){
+  console.log(data);
+}
  
