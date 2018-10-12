8488f76410eb07410f65d41ebf1ffa7ecd73ab9f
Andrew <andrewpulley@gmail.com>
2018-10-11 20:05:24 -0500

    updates

---
+++ scripts/prefix.js
@@ -0,0 +1,101 @@
+const fs = require('fs');
+const path = require('path');
+const concat = require('concat-stream');
+const glob = require('glob');
+const toml = require('@iarna/toml');
+const beautify = require('js-beautify').js;
+
+const rxFileName = /([^\/]+)(?=\.\w+$)/;
+let contentJson = {};
+
+console.log(process.argv[2]);
+
+glob('toml/*.toml', {
+  nodir: true,
+}, function(err, files){
+    console.log(files)
+    let parsedFiles = files.map( str => { 
+      let parts = str.split('/');
+      console.log(parts[parts.length-1]);
+      return parts[parts.length-1].match(/^(.*)(_.*?)$/) ? parts[parts.length-1].match(/^(.*)(_.*?)$/)[1] : '';
+    });
+    console.log(parsedFiles)
+    //buildModules(files.filter(str => {str.match(/^(.*)(_.*?)$/)[1]}), 'content',  myFunction);
+});
+
+// /**
+//  * Parse toml files from array of paths and output into a directory. 
+//  *
+//  * @param {Array} tomlPaths
+//  * @param {String} dir
+//  * @param {Function} callback
+//  */
+// function buildModules(tomlPaths, dir, callback){
+//   let files = [];
+//   const actions = tomlPaths.map(path => {
+//     return new Promise(function(resolve, reject){
+//       fs.createReadStream(path, 'utf8').pipe(concat( data => {
+//         let content = beautify(
+//           `${JSON.stringify(toml.parse(data))}`,
+//           { end_with_newline: true, indent_size: 2, space_in_empty_paren: true}
+//         );
+//         const fileName = path.match(rxFileName)[0];
+//         if (!fs.existsSync(dir)){
+//           fs.mkdirSync(dir);
+//         }
+//         fs.writeFile(
+//           `${dir}/${fileName}.js`, 
+//           `export default ${content}`, 
+//           {flag: 'w'}, 
+//           error => { 
+//             if (error) {
+//               reject(error);
+//             }
+//             resolve();
+//           }
+//         );
+//         files.push({content, fileName});
+//       }));
+//     });
+//   });
+//   Promise.all(actions).then(() => {
+//     let jsonObj = {};
+//     let json = '';
+//     let moduleExport = '';
+    
+//     files.forEach( jsModule => {
+//       moduleExport += `export { default as ${jsModule.fileName} } from './${jsModule.fileName}.js'\n`;
+//       jsonObj[jsModule.fileName] = JSON.parse(jsModule.content);
+//     });
+//     json = beautify(
+//       JSON.stringify(jsonObj),
+//       { end_with_newline: true, indent_size: 2, space_in_empty_paren: true}
+//     );
+//     fs.writeFile(
+//       `${dir}/index.js`, 
+//       moduleExport,
+//       {flag: 'w'}, 
+//       error => {
+//         if (error) {
+//           console.log('error');
+//         }
+//       }
+//     );
+//     fs.writeFile(
+//       `${dir}/content.json`, 
+//       json, 
+//       {flag: 'w'}, 
+//       error => {
+//         callback(jsonObj);
+//         if (error) {
+//           console.log('error');
+//         }
+//       }
+//     );
+
+//   });
+// }
+
+function myFunction(data){
+  console.log('done', data);
+}
